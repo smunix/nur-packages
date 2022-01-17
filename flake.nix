3a0b18c6dd44b28b926102d96918cc63011da434
Providence Salumu <Providence.Salumu@smunix.com>
2022-01-16 21:02:48 -0500

    dtrx: add dtrx package

--- flake.nix
+++ flake.nix
@@ -1,8 +1,13 @@
 {
-  description = "My personal NUR repository";
+  description = "sMuNiX NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
+  inputs.dtrx = {
+    url = "github:dtrx-py/dtrx?ref=master";
+    flake = false;
+  };
+  outputs = { self, nixpkgs, dtrx }:
     let
+      config = { allowUnfree = true; };
       systems = [
         "x86_64-linux"
         "i686-linux"
@@ -11,11 +16,51 @@
         "armv6l-linux"
         "armv7l-linux"
       ];
+      overlay = final: _:
+        with final;
+        with python3Packages;
+        let
+          archivers = lib.makeBinPath [
+            gnutar
+            lhasa
+            rpm
+            binutils
+            cpio
+            gzip
+            p7zip
+            cabextract
+            unshield
+            unzip
+            unrar
+            bzip2
+            xz
+            lzip
+          ];
+        in {
+          dtrx = buildPythonApplication {
+            pname = "dtrx";
+            version = dtrx.shortRev;
+            src = dtrx;
+            propagatedBuildInputs = [ setuptools twine ];
+            postInstall = ''
+              wrapProgram "$out/bin/dtrx" --prefix PATH : "${archivers}"
+            '';
+            doCheck = true;
+            meta = with lib; {
+              description =
+                "Do The Right Extraction: A tool for taking the hassle out of extracting archives";
+              homepage = "https://github.com/dtrx-py/dtrx/";
+              license = licenses.gpl3Plus;
+              platforms = platforms.all;
+            };
+          };
+        };
+      overlays = [ overlay ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
-    {
-      packages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
+    in {
+      inherit overlay;
+      packages = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system config overlays; };
+        in { inherit (pkgs) dtrx; });
     };
 }
